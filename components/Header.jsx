60136f633005b160e8e590b77cce9a98c92cd03c
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2022-05-28 23:21:01 +0000

    upds

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,6 +7,12 @@ const Header = ({ title }) => {
       <title>Solomon Owusu-Ansah</title>
       <meta name="description" content="my portfolio" />
       <link rel="icon" href="/memories-Logo.png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+        rel="stylesheet"
+      ></link>
     </Head>
   );
 };
